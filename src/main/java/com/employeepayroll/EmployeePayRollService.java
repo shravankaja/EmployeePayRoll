e0c7cf864c3797eb9e9ce892221cbc9a4963a2bc
shravan-kaja <shravan.kaja@gmail.com>
2021-03-09 19:32:58 +0530

    Added; gradle dependecy files

---
+++ src/main/java/com/employeepayroll/EmployeePayRollService.java
@@ -0,0 +1,8 @@
+package com.employeepayroll;
+
+public class EmployeePayRollService {
+
+    public static void main(String Args[]) {
+        System.out.println("Welcome to Employee PayRoll Servie");
+    }
+}   
