28c28378353d79dc17c106705015dc1d9fcf4b89
shravan-kaja <shravan.kaja@gmail.com>
2021-03-09 19:51:26 +0530

    Added: methods to read and write data to console

--- src/main/java/com/employeepayroll/EmployeePayRollService.java
+++ src/main/java/com/employeepayroll/EmployeePayRollService.java
@@ -1,8 +1,35 @@
 package com.employeepayroll;
 
+import java.util.*;
+
 public class EmployeePayRollService {
+
+    private ArrayList<EmployeeData> employeeDataArrayList;
+
+    public EmployeePayRollService(ArrayList<EmployeeData> employeeDataArrayList) {
+        this.employeeDataArrayList = employeeDataArrayList;
+    }
+
+    public void readEmployeePayRollData(Scanner consoleInput) {
+        System.out.println("Enter Employee Name :");
+        String name = consoleInput.nextLine();
+        System.out.println("Enter Employee Salary :");
+        double salary = consoleInput.nextDouble();
+        System.out.println("Enter Employee Id :");
+        int id = consoleInput.nextInt();
+        employeeDataArrayList.add(new EmployeeData(name, salary, id));
+    }
 
     public static void main(String Args[]) {
         System.out.println("Welcome to Employee PayRoll Servie");
+        ArrayList<EmployeeData> employeePayRollList = new ArrayList<>();
+        EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayRollList);
+        Scanner consoleInput = new Scanner(System.in);
+        employeePayRollService.readEmployeePayRollData(consoleInput);
+        employeePayRollService.writeEmployeeDataToConsole();
     }
-}   
+
+    public void writeEmployeeDataToConsole() {
+        System.out.println("Employee details are :" + employeeDataArrayList);
+    }
+}
