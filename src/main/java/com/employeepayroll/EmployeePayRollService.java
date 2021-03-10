cfda90159af003347e1034b9c13a4f361ca44926
shravan-kaja <shravan.kaja@gmail.com>
2021-03-10 16:59:31 +0530

    Added: method to append to file and also to count number of entries

--- src/main/java/com/employeepayroll/EmployeePayRollService.java
+++ src/main/java/com/employeepayroll/EmployeePayRollService.java
@@ -30,6 +30,7 @@ public class EmployeePayRollService {
         employeePayRollService.readEmployeePayRollData(consoleInput);
         employeePayRollService.writeEmployeeDataToConsole();
         employeePayRollService.writeEmployeeDataToFile();
+        employeePayRollService.readEmployeeDataFromFile();
     }
 
     public void writeEmployeeDataToConsole() {
@@ -44,18 +45,64 @@ public class EmployeePayRollService {
         if (Files.isDirectory(employeePayRollDirectory)) {
             String newFile = employeePayRollDirectory + "/" + "EmployeePayRollDDetails.txt";
             Path newFilePath = Paths.get(newFile);
-            Files.deleteIfExists(newFilePath);
-            Files.createFile(newFilePath);
-            FileWriter writer = new FileWriter(String.valueOf(newFilePath));
-            for (EmployeeData obj : employeeDataArrayList) {
-                writer.write("Employee Id : " + obj.ID + System.lineSeparator());
-                writer.write("Employee Name : " + obj.name + System.lineSeparator());
-                writer.write("Employee Salary : " + obj.salary + System.lineSeparator());
-                writer.write("------------------------------------------------------------------------------");
-                numberOfEntries = numberOfEntries + 1;
+            if (!Files.exists(newFilePath)) {
+                Files.createFile(newFilePath);
+                //FileWriter writer = new FileWriter(String.valueOf(newFilePath));
+                BufferedWriter out = new BufferedWriter(
+                        new FileWriter(String.valueOf(newFilePath)));
+                for (EmployeeData obj : employeeDataArrayList) {
+                    out.write("Employee Id : " + obj.ID + System.lineSeparator());
+                    out.write("Employee Name : " + obj.name + System.lineSeparator());
+                    out.write("Employee Salary : " + obj.salary + System.lineSeparator());
+                    out.write("--EndOfRecord--" + System.lineSeparator());
+                    numberOfEntries = numberOfEntries + 1;
+                }
+                out.close();
+            } else if (Files.exists(newFilePath)) {
+                for (EmployeeData obj : employeeDataArrayList) {
+                    appendStrToFile(String.valueOf(newFilePath), "-" + System.lineSeparator());
+                    appendStrToFile(String.valueOf(newFilePath), "Employee Id : " + obj.ID + System.lineSeparator());
+                    appendStrToFile(String.valueOf(newFilePath), "Employee Name : " + obj.name + System.lineSeparator());
+                    appendStrToFile(String.valueOf(newFilePath), "Employee Salary : " + obj.salary + System.lineSeparator());
+                    appendStrToFile(String.valueOf(newFilePath), "--EndOfRecord--");
+                    numberOfEntries = numberOfEntries + 1;
+                }
             }
-            writer.close();
         }
         return numberOfEntries;
+    }
+
+    public static void appendStrToFile(String fileName, String str) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+            out.write(str);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("exception occoured" + e);
+        }
+    }
+
+
+    public int readEmployeeDataFromFile() throws FileNotFoundException {
+        int numberOfEntries = 0;
+        int count = 0;
+        Path employeePayRollDirectory = Paths.get("E:\\Eclipse_Practise\\EmployeePayRoll\\src\\main\\java\\com\\employeepayroll");
+        File fileObject = employeePayRollDirectory.toFile();
+        File[] listOfFiles = fileObject.listFiles();
+        if (Files.isDirectory(employeePayRollDirectory)) {
+            String newFile = employeePayRollDirectory + "/" + "EmployeePayRollDDetails.txt";
+            Path newFilePath = Paths.get(newFile);
+            if (Files.exists(newFilePath)) {
+                File myObj = new File(String.valueOf(newFilePath));
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    if (data.equals("--EndOfRecord---")) count = count + 1;
+                    System.out.println(data);
+                }
+                myReader.close();
+            }
+        }
+        return numberOfEntries = count + 1;
     }
 }
