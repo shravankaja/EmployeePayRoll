cfa332967bffcb234e81cdbbb08e85c932c340a8
shravan-kaja <shravan.kaja@gmail.com>
2021-03-10 17:01:03 +0530

    Added: test case to check if correct number of entries are returned while reading and writing to file

--- src/test/java/com/employeepayroll/EmployeePayRollFileOperationsTest.java
+++ src/test/java/com/employeepayroll/EmployeePayRollFileOperationsTest.java
@@ -8,14 +8,25 @@ import java.util.*;
 
 public class EmployeePayRollFileOperationsTest {
 
-    @Test
-    void givenEntriesOfEmployeesWeShouldGetBackNoOfEntriesHaveBeenSentToFile() throws IOException {
-        ArrayList<EmployeeData> employeePayRollList = new ArrayList<>();
-        EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayRollList);
-        Scanner consoleInput = new Scanner(System.in);
-        employeePayRollService.readEmployeePayRollData(consoleInput);
-        employeePayRollService.writeEmployeeDataToConsole();
-        int count = employeePayRollService.writeEmployeeDataToFile();
-        Assertions.assertEquals(1, count);
-    }
+	@Test
+	void givenEntriesOfEmployeesWeShouldGetBackNoOfEntriesHaveBeenSentToFile() throws IOException {
+		System.out.println("Welcome to Employee PayRoll Servie");
+		ArrayList<EmployeeData> employeePayRollList = new ArrayList<>();
+		EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayRollList);
+		Scanner consoleInput = new Scanner(System.in);
+		employeePayRollService.readEmployeePayRollData(consoleInput);
+		employeePayRollService.writeEmployeeDataToConsole();
+		employeePayRollService.writeEmployeeDataToFile();
+		int count = employeePayRollService.writeEmployeeDataToFile();
+		Assertions.assertEquals(2, count);
+	}
+
+	@Test
+	void givenFileToReadWeShouldBeAbleToObtainNoofEntries() throws FileNotFoundException {
+		ArrayList<EmployeeData> employeePayRollList = new ArrayList<>();
+		EmployeePayRollService employeePayRollService = new EmployeePayRollService(employeePayRollList);
+		Scanner consoleInput = new Scanner(System.in);
+		int count = employeePayRollService.readEmployeeDataFromFile();
+		Assertions.assertEquals(2, count);
+	}
 }
